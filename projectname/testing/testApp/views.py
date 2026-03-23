b6458d7c4ce4f852d84e69b63faef4404fd8a1b3
Daniel07070 <130100177+Daniel07070@users.noreply.github.com>
2026-03-23 03:48:17 +0000

    Working search bar functionality(views) and change to home.html

--- projectname/testing/testApp/views.py
+++ projectname/testing/testApp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
+from .models import Chord
 
 
 def home(request):
-	return render(request, 'home.html',{})
+	query = request.GET.get('q')
+	chords = Chord.objects.all() #gets all the chords in the database
+
+	if query: # if user typed something in search then
+		chords = chords.filter(name_icontains=query)
+
+	return render(request, 'home.html',{'chords': chords}) #makes chords available for template
 
 
-# Create your views here.
