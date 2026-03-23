d2a94891c24211356ec902d867f6b1143e3886c6
Daniel07070 <130100177+Daniel07070@users.noreply.github.com>
2026-03-23 04:19:51 +0000

    Deleted extra search button and fixed views

--- projectname/testing/testApp/views.py
+++ projectname/testing/testApp/views.py
@@ -4,10 +4,11 @@ from .models import Chord
 
 def home(request):
 	query = request.GET.get('q')
-	chords = Chord.objects.all() #gets all the chords in the database
 
 	if query: # if user typed something in search then
-		chords = chords.filter(name_icontains=query)
+		chords = Chord.objects.filter(name__icontains=query)
+	else:
+		chords = Chord.objects.none()
 
 	return render(request, 'home.html',{'chords': chords}) #makes chords available for template
 
