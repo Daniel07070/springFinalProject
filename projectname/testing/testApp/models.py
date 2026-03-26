3bafe468dc6199c4422b455bea2522ace001a223
BRAYWAYDOOR <147653436+BRAYWAYDOOR@users.noreply.github.com>
2026-03-26 21:18:00 +0000

    added images

--- projectname/testing/testApp/models.py
+++ projectname/testing/testApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Chord(models.Model):
     name = models.CharField(max_length = 50)
     notes = models.CharField(max_length = 100)
-
+    image = models.ImageField(upload_to='chord_images', blank=True, null=True)
 
     def __str__(self):
         return self.name # This part is for formatting
