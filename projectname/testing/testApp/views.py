92442e2525150625a3f120a8b89b9184bd82109b
ntrevi53 <148792971+ntrevi53@users.noreply.github.com>
2026-04-02 20:13:08 +0000

    added login and register page

--- projectname/testing/testApp/views.py
+++ projectname/testing/testApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from .models import Chord
 
 
@@ -12,4 +13,14 @@ def home(request):
 
 	return render(request, 'home.html',{'chords': chords}) #makes chords available for template
 
+def register(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Account created successfully")
+			return redirect ("login")
+	else:
+		form = UserCreationForm()
 
+	return render(request, "register.html", {"form":form})
