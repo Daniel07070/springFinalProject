e9632ef0970c0159b557610c1953bae66f9a6514
Daniel07070 <130100177+Daniel07070@users.noreply.github.com>
2026-04-07 02:57:07 +0000

    Added new model for multiple images per chord

--- projectname/testing/testApp/admin.py
+++ projectname/testing/testApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Chord
+from .models import Chord, ChordImage
 
 admin.site.register(Chord)
+admin.site.register(ChordImage)
 
 
--- projectname/testing/testApp/models.py
+++ projectname/testing/testApp/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Chord(models.Model):
     name = models.CharField(max_length = 50)
     notes = models.CharField(max_length = 100)
-    image = models.ImageField(upload_to='chord_images', blank=True, null=True)
 
     def __str__(self):
         return self.name # This part is for formatting
+class ChordImage(models.Model):
+    chord = models.ForeignKey(Chord, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='chord_images/')
 
+    def __str__(self):
+        return f"Image for {self.chord.name}"
